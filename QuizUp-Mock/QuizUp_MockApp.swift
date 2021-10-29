353b3210a8540e9871b791ee662325c56c95bba0
Edwin Bosire <me@edwinb.co.uk>
2021-10-29 23:15:24 +0100

    Initial Commit

---
+++ QuizUp-Mock/QuizUp_MockApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizUp_MockApp.swift
+//  QuizUp-Mock
+//
+//  Created by Edwin Bosire on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct QuizUp_MockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
